5510f29746647f3678d335a75ba16000436aac6e
deaf52 <eorms96@naver.com>
2024-01-02 14:24:49 +0900

    fix(User) : 오류나는 test파일 출력문 추가

--- User/src/test/java/org/scalablemsa/user/UserApplicationTests.java
+++ User/src/test/java/org/scalablemsa/user/UserApplicationTests.java
@@ -8,6 +8,12 @@ class UserApplicationTests {
 
 	@Test
 	void contextLoads() {
+		try {
+			// 컨텍스트 로드 관련 작업을 수행하는 코드
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e; // 예외를 다시 던져서 테스트가 실패했음을 나타냄
+		}
 	}
 
 }
