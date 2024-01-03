9abeb902c5395a6f9b7f3ae409bf02a881474648
deaf52 <eorms96@naver.com>
2024-01-03 18:13:55 +0900

    test(User) : 빌드파일 실행하는지 로그추가

--- User/deploy-user.sh
+++ User/deploy-user.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/myapp
-cd $REPOSITORY
+LOG_FILE=$REPOSITORY/log.txt
+
+cd $REPOSITORY || echo "repository 없음 $REPOSITORY" | tee -a $LOG_FILE
 
 APP_NAME=ScalableMSA-User
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
@@ -9,14 +11,14 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
+if [ -z "$CURRENT_PID" ]
 then
-  echo "> 종료할것 없음."
+  echo "> 종료할 것 없음." | tee -a $LOG_FILE
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
+  echo "> kill -9 $CURRENT_PID" | tee -a $LOG_FILE
+  kill -9 "$CURRENT_PID"
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_PATH 배포" | tee -a $LOG_FILE
+nohup java -jar -Dspring.profiles.active=prod "$JAR_PATH" > /dev/null 2> /dev/null < /dev/null &
