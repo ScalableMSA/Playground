7079d67b25d761cfea338c0a61996b7487710a93
deaf52 <eorms96@naver.com>
2024-01-03 18:25:47 +0900

    test(User) : deploy-user.sh 실행여부 테스트 롤백

---
+++ User/deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+REPOSITORY=/home/ubuntu/myapp
+LOG_FILE=$REPOSITORY/log.txt
+
+echo "deploy.sh 시작" | sudo tee -a $LOG_FILE
+
+cd $REPOSITORY || echo "repository 없음 $REPOSITORY" | sudo tee -a $LOG_FILE
+
+APP_NAME=ScalableMSA-User
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z "$CURRENT_PID" ]
+then
+  echo "> 종료할 것 없음." | sudo tee -a $LOG_FILE
+else
+  echo "> kill -9 $CURRENT_PID" | sudo tee -a $LOG_FILE
+  kill -9 "$CURRENT_PID"
+  sleep 5
+fi
+
+echo "> $JAR_PATH 배포" | sudo tee -a $LOG_FILE
+nohup java -jar -Dspring.profiles.active=prod "$JAR_PATH" > /dev/null 2> /dev/null < /dev/null &
